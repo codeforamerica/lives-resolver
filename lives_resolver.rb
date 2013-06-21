4533d590f609faf3c388dcc31baa1ced5665587b
Dan Avery <dan.github@danavery.com>
2013-06-21 11:57:11 -0700

    Allow for name spread across multiple fields.

--- lives_resolver.rb
+++ lives_resolver.rb
@@ -18,12 +18,17 @@ class LivesResolver
   end
 
   def resolve_csv(file_path)
-    factual = Factual.new(ENV["FACTUAL_KEY"], ENV["FACTUAL_SECRET"], debug: true)
+    factual = Factual.new(ENV["FACTUAL_KEY"], ENV["FACTUAL_SECRET"], debug: false)
     counter = 1
     last_id = nil
     CSV.foreach(file_path, :headers => :true) do |row|
       if ((row[@local_id_field] != last_id) && (@@coll.find("id"=>row[@local_id_field]).to_a.count == 0))
-        hash_input = {  name: row[@name_field], 
+        name_array = []
+        @name_fields.each do |name_field|
+          name_array.push(row[name_field])
+        end
+        name = name_array.join(" ")
+        hash_input = {  name: name, 
                         address: row[@address_field], 
                         locality: row[@locality_field] || @locality_default, 
                         region: row[@region_field] || @region_default, 
@@ -33,19 +38,19 @@ class LivesResolver
           query = factual.resolve(hash_input)
           factual_response = query.first
           if query.first == nil
-            p "#{counter}: nil response from Factual"
+            puts "#{counter}: nil response from Factual"
             #binding.pry
           else
-            p "#{counter}: hit!"
+            puts "#{counter}: hit!"
           end
         rescue Timeout::Error
-          p "#{counter} Timeout Error"
+          puts "#{counter} Timeout Error"
           factual_response = "factual-timeout"
         end
         insert_hash = { id: lives_id, lives_data: hash_input, factual_data: factual_response }
         test_insert = @@coll.insert(insert_hash)
       else
-        p "#{counter} skipping -- already done (or to be skipped)"
+        puts "#{counter} skipping -- already done (or to be skipped)"
       end
       counter += 1
       last_id = row[@local_id_field]
@@ -77,14 +82,14 @@ end
 class KingCountyLivesResolver < LivesResolver
   def initialize()
     @local_id_field = "Business_ID"
-    @name_field = "Name"
+    @name_fields = ["Program Identifier"]
     @address_field = "Address"
     @locality_field = "City"
     @region_field = "State"
     @region_default = "WA"
     @postcode_field = "Zip Code"
     @mongo_db_name = "factual-test"
-    @mongo_coll_name = "factual-data-king-2"
+    @mongo_coll_name = "factual-data-king-program-identifier"
     super
   end
 end
@@ -92,7 +97,7 @@ end
 class SFLivesResolver < LivesResolver
   def initialize()
     @local_id_field = "id"
-    @name_field = "name"
+    @name_fields = ["name"]
     @address_field = "address1"
     @locality_field = "" # SF data doesn't have this field, so supply a default
     @locality_default = "San Francisco"
@@ -106,7 +111,7 @@ class SFLivesResolver < LivesResolver
 end
 
 lr = KingCountyLivesResolver.new
-lr.resolve_csv("./Food_Establishment_Inspection_Data.csv")
+lr.resolve_csv("king_county/Food_Establishment_Inspection_Data.csv")
 
 #l = LivesResolver.new
 #l.output_unmatched_to_json("./lives-sf.csv", "./unmatched-LIVES-SF-restaurants.csv")
